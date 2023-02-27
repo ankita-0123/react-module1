7a048b738b0c6a4233e8297dcd5fb1082bd84a8e
ankita-0123 <pragyanparamita1449@gmail.com>
2023-02-27 19:11:03 +0530

    commit id

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 import Header from "./components/Layout/Header";
 
@@ -7,10 +7,20 @@ import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [showCart, setShowCart] = useState(false)
+  
+
+  const showCartHandler = () => {
+    setShowCart(true)
+  }
+
+  const hideCartHandler = () => {
+    setShowCart(false)
+  }
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {showCart &&<Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
 
